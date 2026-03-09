efb33e9690935cac87415a69f15d283a0d76b154
SyedMas8d <syedmasood.s@foodhub.com>
2026-03-09 06:34:48 +0530

    20 & 232

---
+++ stack_n_queues/20_valid_paratheses.js
@@ -0,0 +1,63 @@
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+
+// Example 1:
+
+// Input: s = "()"
+
+// Output: true
+
+// Example 2:
+
+// Input: s = "()[]{}"
+
+// Output: true
+
+// Example 3:
+
+// Input: s = "(]"
+
+// Output: false
+
+// Example 4:
+
+// Input: s = "([])"
+
+// Output: true
+
+// Example 5:
+
+// Input: s = "([)]"
+
+// Output: false
+
+// Constraints:
+
+// 1 <= s.length <= 104
+// s consists of parentheses only '()[]{}'.
+
+function isValidParanthese(s) {
+    const parenthesesInverse = {
+        ')': '(',
+        ']': '[',
+        '}': '{',
+    };
+    const queue = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (queue.length && queue[queue.length - 1] == parenthesesInverse[s[i]]) {
+            queue.pop();
+        } else {
+            queue.push(s[i]);
+        }
+    }
+
+    return queue.length ? false : true;
+}
+
+console.log(isValidParanthese('(('));
